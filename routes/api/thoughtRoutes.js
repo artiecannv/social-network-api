eace91402c0adce9c1765dadc6944d3c3eaaad66
artiecannv <thefiftharthur@gmail.com>
2023-03-22 21:26:17 -0600

    thoughtRoutes finished up

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,25 @@
 const router = require("express").Router();
+const {
+  getThoughts,
+  createThought,
+  getOneThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtController");
+
+// Routes - /api/thoughts, /api/thoughts/:thoughtId, /api/thoughts/:thoughtId/:reactions, & /api/thoughts/:thoughtId/reactions
+router.route("/").get(getThoughts).post(createThought);
+
+router
+  .route("/:thoughtId")
+  .get(getOneThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(createReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+module.exports = router;
